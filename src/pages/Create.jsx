1648ba0650f970cb0b567fd31b0f6e7eb479af22
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-10-02 18:22:22 +0000

    Criação do `<form>`

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,3 +1,23 @@
 export default function Create() {
-  return <div>Create</div>
+  return <div>
+    <h1>Criar DevMon</h1>
+    <form>
+      <div>
+        <label htmlFor="nome">Nome*: </label>
+        <input type="text" id="nome" name="nome" placeholder="Digite o nome" />
+      </div>
+      <div>
+        <label htmlFor="imagem">Imagem (URL)*: </label>
+        <input type="text" id="imagem" name="imagem" placeholder="Insira a URL da imagem" />
+      </div>
+      <div>
+        <label htmlFor="evoluiPara">Evolui Para: (Opcional)</label>
+        <input type="text" id="evoluiPara" name="evoluiPara" placeholder="Insira o nome para evolução" />
+      </div>
+      <div>
+        <button type="submit" >Criar</button>
+        <button type="reset" >Reiniciar</button>
+      </div>
+    </form>
+  </div>
 }
