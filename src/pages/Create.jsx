58e265bbb1d350cf765ef19e9cf3cfcfdfe8c644
José Luiz <36302287+joseluizs@users.noreply.github.com>
2024-10-02 23:53:52 +0000

    Api implementada na página de `Create`

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,7 +1,34 @@
+import { toast } from "react-toast"
+import { Api } from "../api/api"
+import { useNavigate } from "react-router-dom"
+
 export default function Create() {
+  
+  const navigate = useNavigate()
+  async function handleSubmit(event){
+    event.preventDefault()
+    
+    const devmon = {
+      nome: event.target.nome.value,
+      imagem: event.target.imagem.value,
+      evoluiPara: event.target.evoluiPara.value,
+    }
+
+    const apiUrl = Api.personagem.create()
+    const response = await Api.buildApiPostRequest(apiUrl, devmon)
+
+    if (response.ok) {
+      toast.success('DevMon criado com sucesso!')
+      navigate('/')
+    } else {
+      const body = await response.json()
+      toast.error('Error ao criar DevMon ' + body.error)
+    }
+  }
+
   return <div>
     <h1>Criar DevMon</h1>
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="nome">Nome*: </label>
         <input type="text" id="nome" name="nome" placeholder="Digite o nome" />
